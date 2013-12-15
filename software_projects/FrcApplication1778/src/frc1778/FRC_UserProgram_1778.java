58443cce05eec461467edb6a3d3e311de5808549
Rob Trahms <rtrahms@gmail.com>
2013-12-15 10:15:45 -0800

    Added three other WPI starter template projects
    IterativeRobot Template
    FRC_Application Template
    CommandBasedRobot Template

---
+++ software_projects/FrcApplication1778/src/frc1778/FRC_UserProgram_1778.java
@@ -0,0 +1,29 @@
+package frc1778;
+
+/*
+ * StartApplication.java
+ *
+ */
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+/**
+ * The startApp method of this class is called by the VM to start the
+ * application.
+ * 
+ * The manifest specifies this class as MIDlet-1, which means it will
+ * be selected for execution.
+ */
+public class FRC_UserProgram_1778 extends MIDlet {
+
+    protected void startApp() throws MIDletStateChangeException {
+        System.out.println("Hello World");
+    }
+
+    protected void pauseApp() {
+    }
+
+    protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+    }
+    
+}
