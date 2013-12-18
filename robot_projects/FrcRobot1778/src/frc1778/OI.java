b1298023d91e318accbe798cd723d984c8889d97
Rob Trahms <rtrahms@gmail.com>
2013-12-17 16:58:05 -0800

    Updated first robot project

--- robot_projects/FrcRobot1778/src/frc1778/OI.java
+++ robot_projects/FrcRobot1778/src/frc1778/OI.java
@@ -3,42 +3,43 @@ package frc1778;
 
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+import frc1778.commands.*;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
     
-    // Another type of button you can create is a DigitalIOButton, which is
-    // a button or switch hooked up to the cypress module. These are useful if
-    // you want to build a customized operator interface.
-    // Button button = new DigitalIOButton(1);
+    // left joystick and buttons
+    Joystick leftJoy = new Joystick(1);
+    Button leftButton1 = new JoystickButton(leftJoy,1);
+    Button leftButton2 = new JoystickButton(leftJoy,2);
+    Button leftButton3 = new JoystickButton(leftJoy,3);
+    Button leftButton4 = new JoystickButton(leftJoy,4);
+    Button leftButton5 = new JoystickButton(leftJoy,5);
+    Button leftButton6 = new JoystickButton(leftJoy,6);
+    Button leftButton7 = new JoystickButton(leftJoy,7);
+    Button leftButton8 = new JoystickButton(leftJoy,8);
     
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
+    Joystick rightJoy = new Joystick(2);
+    Button rightButton1 = new JoystickButton(rightJoy,1);
+    Button rightButton2 = new JoystickButton(rightJoy,2);
+    Button rightButton3 = new JoystickButton(rightJoy,3);
+    Button rightButton4 = new JoystickButton(rightJoy,4);
+    Button rightButton5 = new JoystickButton(rightJoy,5);
+    Button rightButton6 = new JoystickButton(rightJoy,6);
+    Button rightButton7 = new JoystickButton(rightJoy,7);
+    Button rightButton8 = new JoystickButton(rightJoy,8);
+   
+    // constructor
+    public OI() {
+        leftButton1.whenPressed(new LowerScoop());
+        leftButton1.whenReleased(new RaiseScoop());
+    }
     
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
 }
 
