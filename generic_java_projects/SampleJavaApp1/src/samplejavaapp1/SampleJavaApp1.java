5ee48c672e81730f5f63cbcc946b00f883fbb45a
rtrahms <rtrahms@gmail.com>
2014-01-08 17:09:28 -0800

    Added generic java project section (for tutorials)

---
+++ generic_java_projects/SampleJavaApp1/src/samplejavaapp1/SampleJavaApp1.java
@@ -0,0 +1,26 @@
+/*
+ * Simple Java Application #1
+ * Show some Java basics
+ * NOT a FIRST robot program
+ */
+
+package samplejavaapp1;
+
+/**
+ *
+ * @author Rob
+ */
+public class SampleJavaApp1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // A simple console output program
+        // If running this inside nebeans or other IDE,
+        // make sure you have your output window open
+        
+        System.out.println("Hello World!");
+    }
+    
+}
