f80ef1ab7c98319deb9365b8ee698b3eb4b8e9f0
dlaxis42 <davidhudson42@me.com>
2014-02-08 13:27:01 -0800

    Roller Subsystem
    I have the untested roller subsystem here. Have fun. That is all.

---
+++ robot_projects/Icebox/src/frc1778/subsystems/Roller.java
@@ -0,0 +1,50 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frc1778.subsystems;
+
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc1778.commands.RollerOp;
+import frc1778.RobotMap;
+
+/**
+ *
+ * @author hudsodav000
+ */ 
+public class Roller extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    
+    CANJaguar roller;
+    RobotMap rMap;
+    
+    public Roller() {
+        super("Roller");
+        rMap = new RobotMap();
+        rMap.DBG("Roller");
+    }
+    
+    public void setRollerSpeed(double spd){
+        System.out.println("setRollerSpeed" + spd);
+        try {
+            roller.setX(spd, (byte) 0);
+        } catch (CANTimeoutException e) {
+            System.out.println("X setLeftRight");
+        }
+    }
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new RollerOp());
+    }
+    
+    public void setSafety(boolean enabled) {
+        rMap.DBG("setSafety");
+        roller.setSafetyEnabled(enabled);
+    }
+}
