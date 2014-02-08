affea0526d56851b690167b32acbde4353c30c2c
ParaXtHetA <persontheguy123@gmail.com>
2014-02-08 13:30:40 -0800

    Fixed Roller configuration in OI

--- robot_projects/Icebox/src/frc1778/commands/CommandBase.java
+++ robot_projects/Icebox/src/frc1778/commands/CommandBase.java
@@ -1,16 +1,16 @@
 package frc1778.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc1778.OI;
-import frc1778.RobotClass;
 import frc1778.subsystems.Drive;
+import frc1778.subsystems.Roller;
 
 public abstract class CommandBase extends Command {
 
     public static OI oi;
     // Create a single static instance of all of your subsystems
     public static Drive drive;
+    public static Roller roller;
 
     // TODO: Declare a single static instance of front gate
     // TODO: Matt should do this
