b66ea734b11266819e9a8f635ade1e273b8b812d
rtrahms <rtrahms@gmail.com>
2014-10-09 21:56:13 -0700

    Updated Cannon Control
    Relays working for first time - need to fix sequencing, and re-enable
    tank drive

--- CannonControl/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ CannonControl/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -17,16 +17,20 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 // This is the RobotTemplate class. I guess.
 public class RobotTemplate extends SimpleRobot {
     
     // Declare Objects
+    /*
     CANJaguar  mFrontLeft;
     CANJaguar  mFrontRight;
     CANJaguar  mBackLeft;
     CANJaguar  mBackRight;
+    
     RobotDrive drive;
+    */
     Joystick   leftStick;
     Joystick   rightStick;
     Relay[]    cannonTrigger;
@@ -47,26 +51,32 @@ public class RobotTemplate extends SimpleRobot {
     
     public RobotTemplate() throws CANTimeoutException {
         // Initialize Objects
+        /*
         mFrontLeft    = new CANJaguar(5);
         mBackLeft     = new CANJaguar(6);
         mFrontRight   = new CANJaguar(7);
         mBackRight    = new CANJaguar(8);
+        */
         leftStick     = new Joystick(1);
         rightStick    = new Joystick(2);
+        
         cannonTrigger = new Relay[3];
-        lightOne = new Relay(1, 1);
-        lightTwo = new Relay(1, 2);
-        cannonTrigger[0] = new Relay(2, 1, Relay.Direction.kForward);
-        cannonTrigger[1] = new Relay(2, 2, Relay.Direction.kForward);
-        cannonTrigger[2] = new Relay(2, 3, Relay.Direction.kForward);
+        //lightOne = new Relay(2, 4, Relay.Direction.kReverse);
+        //lightTwo = new Relay(2, 5, Relay.Direction.kReverse);
+        // MODULE 1
+        cannonTrigger[0] = new Relay(1, 1);
+        cannonTrigger[1] = new Relay(1, 2);
+        cannonTrigger[2] = new Relay(1, 3);
         barrelCount = 0;
         
         // Initialize RobotDrive class as drive
+        /*
         drive = new RobotDrive(mFrontLeft, mBackLeft, mFrontRight, mBackRight);
         drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
         drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
         drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
         drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+        */
     }
     
     // There was an autonomous code here. It's gone now.
@@ -74,27 +84,42 @@ public class RobotTemplate extends SimpleRobot {
     // Operator controlled period
     public void operatorControl() {
         getWatchdog().setEnabled(false);
+        //lightOne.set(Relay.Value.kOn);
+        //lightTwo.set(Relay.Value.kOn);
         //display.clear();
         //gyro.reset();
         while(isEnabled() && isOperatorControl()) {
             //double angle = gyro.getAngle();
-            drive.tankDrive(leftStick, rightStick);
+            //drive.tankDrive(leftStick, rightStick);
             //display.println(DriverStationLCD.Line.kUser1,1,Double.toString(angle));
             //display.updateLCD();
             
             // If both triggers are pressed and the barrel isn't open
             if(leftStick.getButton(Joystick.ButtonType.kTrigger) && rightStick.getButton(Joystick.ButtonType.kTrigger) && !barrelOpen){ //verify that both triggers are pressed
+            //if (leftStick.getButton(Joystick.ButtonType.kTrigger)) { //verify that both triggers are pressed
                 startTime = Timer.getFPGATimestamp();
                 barrelOpen = true;
+                System.out.println("triggered - barrelCount = " + barrelCount);
                 cannonTrigger[barrelCount].set(Relay.Value.kForward);
             }
             
             // Ensures the Barrel is open for three seconds
+            
             if(Timer.getFPGATimestamp() - startTime >= barrelOpenSec){
                 barrelOpen = false;
                 cannonTrigger[barrelCount].set(Relay.Value.kOff);
                 barrelCount = (barrelCount+1) % 3;
             }
+            
+        }
+        //lightOne.set(Relay.Value.kOff);
+        //lightTwo.set(Relay.Value.kOff);
+    }
+    
+    public void test() {
+        while(isTest() && isEnabled()) {
+            LiveWindow.run();
+            Timer.delay(0.1);
         }
     }
     
