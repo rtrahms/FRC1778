717cb4e572cfca76024e266bb0da18d4c6f59adf
Scott Davidson <scottd@mcneel.com>
2016-07-18 18:19:16 -0700

    Added Button and Axis mapping for Logitech DualAction Controller
    Various Logitech controllers have different mapping.  This is for the
    DualAction Controller.

---
+++ ProtoBot/src/Utility/LogitechDualAction.java
@@ -0,0 +1,24 @@
+package Utility;
+
+public class LogitechDualAction {
+	public static final int DPAD = 0;//GetPOV () direction for directional gamepad
+	public static final int B1 = 1;//1 Button group left
+	public static final int B2 = 2;//2 Button group down
+	public static final int B3 = 3;//3 Button group right
+	public static final int B4 = 4;//4 Button group up
+	public static final int LEFT_BUMPER = 5;
+	public static final int RIGHT_BUMPER = 6;
+	public static final int LEFT_TRIGGER = 7;
+	public static final int RIGHT_TRIGGER = 8;
+	public static final int SELECT = 9;//Top middle button on left
+	public static final int START = 10;//Top middle button on right
+	public static final int LEFT_PRESS_JOYSTICK = 11;
+	public static final int RIGHT_PRESS_JOYSTICK = 12;
+
+	public class Axis {
+		public static final int LEFT_X = 0;
+		public static final int LEFT_Y = 1;
+		public static final int RIGHT_X = 2;
+		public static final int RIGHT_Y = 3;
+	}
+}
