6fe0ed71e34c9aea37756d843786bd0ebdfc0556
rtrahms <rtrahms@gmail.com>
2016-07-31 09:15:41 -0700

    Updated ChillOutObjTracker code
    Reflects 2016 GRIP processing directly in OpenCV, ie
    threshold/contour/convex hull code.  Sends target info directly to
    Roborio via network table.

--- RPi_Vision_2016/chillOutObjTracker.cpp
+++ RPi_Vision_2016/chillOutObjTracker.cpp
@@ -18,6 +18,8 @@ int main()
     	SimpleBlobDetector::Params params;
 
 	FILE *parameter_file = NULL;
+
+	char roborio_ipaddr[32];
 	
 	/********* OpenCV parameter section *******/
 	int frameWidth =  640;
@@ -43,6 +45,11 @@ int main()
 	params.filterByInertia = false;
 	params.minInertiaRatio = 0.01;
 
+	// minimum target radius
+	int minRadius = 10;
+	int targetIndex = -1;
+	bool targetDetected = false;
+
 	// color filter params
 	int minColor_h = 30;
 	int minColor_s = 50;
@@ -59,6 +66,7 @@ int main()
 	parameter_file = fopen("chillOut_params.txt","r");
 	if (parameter_file != NULL) {
 		printf("Reading parameters from file\n");
+		fscanf(parameter_file,"roborio_ipaddr = %s\n",roborio_ipaddr);
 		fscanf(parameter_file,"frameWidth = %d\n",&frameWidth);
 		fscanf(parameter_file,"frameHeight = %d\n",&frameHeight);
 		fscanf(parameter_file,"minArea = %f\n",&(params.minArea));
@@ -66,6 +74,7 @@ int main()
 		fscanf(parameter_file,"minCircularity = %f\n",&(params.minCircularity));
 		fscanf(parameter_file,"minConvexity = %f\n",&(params.minConvexity));
 		fscanf(parameter_file,"minInertiaRatio = %f\n",&(params.minInertiaRatio));
+		fscanf(parameter_file,"minRadius = %d\n",&minRadius);
 		fscanf(parameter_file,"minColor_h = %d\n",&minColor_h);
 		fscanf(parameter_file,"minColor_s = %d\n",&minColor_s);
 		fscanf(parameter_file,"minColor_v = %d\n",&minColor_v);
@@ -75,6 +84,7 @@ int main()
 	}
 	fclose(parameter_file);
 	printf("File read complete.\n");
+	printf("roborio_ipaddr = %s\n",roborio_ipaddr);
 	printf("frameWidth = %d\n",frameWidth);
 	printf("frameHeight = %d\n",frameHeight);
 	printf("minArea = %f\n",params.minArea);
@@ -93,7 +103,7 @@ int main()
 	// initialize network table for comm with the robot
 	
 	static std::shared_ptr<NetworkTable> table;
-	NetworkTable::SetIPAddress("10.17.78.77");
+	NetworkTable::SetIPAddress(roborio_ipaddr);
 	NetworkTable::SetClientMode();
 	NetworkTable::Initialize();
 	table = NetworkTable::GetTable("RPIComm/Data_Table");
@@ -111,42 +121,115 @@ int main()
     // capture loop - runs forever
     while( cap.isOpened() )
     {
-        Mat frame,src_gray,hsv,binary, frame_with_keys;
-        if ( ! cap.read(frame) )
+        Mat inputImg, src_gray, hsvImg, binaryImg, frame_with_keys;
+	Mat erosionImg, dilationImg, contourImg;
+
+	vector<Vec4i> hierarchy;
+	vector<vector<Point>> contours;
+
+        if ( ! cap.read(inputImg) )
             break;
-       cvtColor( frame, hsv, CV_BGR2HSV );
-	//inRange(hsv, Scalar(20,100, 100), Scalar(30,255, 255), binary);	/*yellow*/
-	//inRange(hsv, Scalar(157,72, 156), Scalar(180,169, 255), binary);	/*pink*/
-	inRange(hsv, Scalar(minColor_h, minColor_s, minColor_v), Scalar(maxColor_h, maxColor_s, maxColor_v), binary);		/*green*/
-	//inRange(hsv, Scalar(40,0, 180), Scalar(135,110, 255), binary);       /*white*/
-      	//inRange(hsv, Scalar(200,0, 0), Scalar(210,255, 255), binary);		/*purple*/
 
-	// Set up detector with params
-	Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);   
+	// color threshold input image into binary image
+        cvtColor( inputImg, hsvImg, CV_BGR2HSV );
+	inRange(hsvImg, Scalar(minColor_h, minColor_s, minColor_v), Scalar(maxColor_h, maxColor_s, maxColor_v), binaryImg);		/*green*/
 
-	std::vector<KeyPoint> keypoints;
-	detector->detect(binary,keypoints);
+	// erode thresholded image - not used
+	/*
+	Mat erosionElement = getStructuringElement(MORPH_RECT, Size(7,7), Point(3,3));
+	erode(binaryImg,erosionImg,erosionElement);	
+	
+	// dilate eroded image
+	Mat dilateElement = getStructuringElement(MORPH_RECT, Size(7,7), Point(3,3));
+	dilate(erosionImg, dilationImg, dilateElement);
+	*/
 
-	//drawKeypoints(binary,keypoints,frame_with_keys,Scalar(0,255,255),DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-	drawKeypoints(frame,keypoints,frame_with_keys,Scalar(0,255,255),DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
- 
-	// write some data to the robot
-	table->PutNumber("targets",(float)keypoints.size());
-	if (keypoints.size() > 0) {
-		table->PutNumber("targetX",keypoints[0].pt.x);
-		table->PutNumber("targetY",keypoints[0].pt.y);
+	// find contours from dilated image, place in list (vector)
+	//findContours(dilationImg, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE,Point(0,0));
+	findContours(binaryImg, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE,Point(0,0));
+
+	// find the convex hull objects
+	vector<vector<Point>> hulls (contours.size());	
+	for (int i=0; i< contours.size(); i++)
+	{
+		convexHull(Mat(contours[i]), hulls[i], false);
 	}
 
-	/*** blob detection output - debug only ***/
-	//printf("keypoints size = %d\n",keypoints.size());
-	//for (int i=0; i< keypoints.size(); i++)
-	//	printf("keypoints[%d]  = (%4.2f,%4.2f)\n",i,keypoints[i].pt.x,keypoints[i].pt.y);
+	// create a bounding rect for each convex hull
+	vector<vector<Point>> contours_poly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	vector<Point2f>center(contours.size());
+	vector<float>radius(contours.size());
+
+	for (int i=0; i<contours.size(); i++)
+	{
+		approxPolyDP(Mat(hulls[i]),contours_poly[i], 3, true);
+		//boundRect[i] = boundingRect(Mat(contours_poly[i]));
+		minEnclosingCircle((Mat)contours_poly[i], center[i], radius[i]);
+	}
+
+	int maxTargetRadius = -1;
+	int targetIndex = -1;
+	targetDetected = false;
+	for (int i=0; i<contours.size(); i++)
+	{
+		// see if this target meets the minimum radius requirement
+		if (radius[i] > minRadius)
+		{
+			targetDetected = true;
+
+			// see if this target is the biggest so far
+			if (radius[i] > maxTargetRadius)
+			{
+				targetIndex = i;
+				maxTargetRadius = radius[i];
+			}
+		}
+	}
+
+	// create contour image
+	contourImg = Mat::zeros(binaryImg.size(),CV_8UC3);
+	/*
+	for (int i=0; i< contours.size(); i++)
+	{
+		Scalar colorGreen = Scalar(0, 0, 255);  // green
+		Scalar colorWhite = Scalar(255, 255, 255);  // white
+		//drawContours(contourImg, contours, i, colorGreen, 1, 8, hierarchy, 0, Point());
+		drawContours(contourImg, hulls, i, colorWhite, 1, 8, hierarchy, 0, Point());
+		circle(contourImg, center[i], (int)radius[i],colorGreen,1,8,0);
+	}
+	*/
+
+	// if target meets criteria, do stuff
+	if (targetDetected)
+	{
+		// write target info out to roborio
+		table->PutNumber("targets",(float)1.0f);
+		table->PutNumber("targetX",center[targetIndex].x);
+		table->PutNumber("targetY",center[targetIndex].y);
+
+		// draw the target on contour image
+		Scalar colorWhite = Scalar(255, 255, 255);  // white
+		circle(contourImg, center[targetIndex], (int)radius[targetIndex],colorWhite,1,8,0);
+
+		printf("Target radius %3.0f detected at (%3.0f,%3.0f)\n",
+			radius[targetIndex], center[targetIndex].x,center[targetIndex].y);
+	}
+	else
+	{
+		// let roborio know that no target is detected
+		table->PutNumber("targets",(float)0.0f);
+		printf("No target\n");
+	}
 	
 
 	/*** Image output code - debug only ****/
-	//imshow("lalala",binary);
-	//imshow("original",frame);
-	imshow("keys",frame_with_keys);
+	//imshow("original",inputImg);
+	//imshow("threshold binary",binaryImg);
+	//imshow("erosionImg",erosionImg);
+	//imshow("dilationImg",dilationImg);
+	imshow("contours",contourImg);
+	//imshow("keys",frame_with_keys);
 
         int k = waitKey(1);
         if ( k==27 )
@@ -154,3 +237,4 @@ int main()
     }
     return 0;
 }
+
